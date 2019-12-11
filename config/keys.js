eb54bca64922ffac55c88acdd75e95303bb6bcd9
Apurva Katti <apurva.katti19894@gmail.com>
2019-12-11 13:05:20 -0800

    folders commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:'616402644951-0jvtc5n5i5gcdckeo6vg464i3va3q1b2.apps.googleusercontent.com',
+    googleClientSecret:'nKNGvCpB_K8pdLEMvPsF6ewS',
+    mongoURI: 'mongodb+srv://ApurvaKatti:Bangalore007@emaily-0fqx6.mongodb.net/test?retryWrites=true&w=majority',
+    //mongoURI:'mongodb+srv://p00gz:JyKbVf7l6ATUk54h@emaily-uwsj6.mongodb.net/student?retryWrites=true',
+    cookieKey:'fdbshjrfkheskjfhdjbfjdbjfhjsgbhgbsfdjhg',
+};
